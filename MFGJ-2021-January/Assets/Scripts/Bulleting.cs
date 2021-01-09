7407cd4cddd04364e27bf1ab5a4f8a49aca58d71
colorplease <alistairvtran@gmail.com>
2021-01-09 11:33:06 -0800

    Shooting and Aiming Implemented

---
+++ MFGJ-2021-January/Assets/Scripts/Bulleting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulleting : MonoBehaviour
+{
+   public float speed;
+   public float lifeTime;
+
+   void Start()
+   {
+       Invoke("DestroyBullet", lifeTime);
+   }
+
+   void Update()
+   {
+       
+   }
+}
