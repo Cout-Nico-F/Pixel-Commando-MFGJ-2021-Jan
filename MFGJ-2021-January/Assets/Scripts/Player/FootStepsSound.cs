b6f67534839114c788ee41d84d97d0423b65be1f
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-10 14:46:33 -0500

    Added Grass Footsteps(FS)

---
+++ MFGJ-2021-January/Assets/Scripts/Player/FootStepsSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepsSound : MonoBehaviour
+{
+    [Header("FootStepsSounds")]
+    public List<AudioClip> footstepsGrass;
+    AudioSource aS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playFootstepsSound()
+    {
+        AudioClip currentFs = footstepsGrass[Random.Range(0,footstepsGrass.Count)];
+        //aS.clip = currentFs;
+        aS.pitch = 1 + Random.Range(-0.1f, 0.1f);
+        aS.volume = 1 - Random.Range(0, 0.7f);
+        aS.PlayOneShot(currentFs);
+
+
+    }
+}
