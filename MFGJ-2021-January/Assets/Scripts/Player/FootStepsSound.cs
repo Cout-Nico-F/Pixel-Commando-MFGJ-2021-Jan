8ab9bbbc899bbacd95375a802803b2d056a99096
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-10 19:17:06 -0500

    Footsteps sounds prototype

--- MFGJ-2021-January/Assets/Scripts/Player/FootStepsSound.cs
+++ MFGJ-2021-January/Assets/Scripts/Player/FootStepsSound.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class FootStepsSound : MonoBehaviour
 {
+    private int audioClipIndex;
+
     [Header("FootStepsSounds")]
     public List<AudioClip> footstepsGrass;
+    public List<AudioClip> footstepsSand;
+    public List<AudioClip> footstepsConcrete;
+    public List<AudioClip> footstepsWater;
+
+
     AudioSource aS;
+    AudioClip currentFs;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +28,37 @@ public class FootStepsSound : MonoBehaviour
         
     }
 
-    void playFootstepsSound()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        AudioClip currentFs = footstepsGrass[Random.Range(0,footstepsGrass.Count)];
-        //aS.clip = currentFs;
-        aS.pitch = 1 + Random.Range(-0.1f, 0.1f);
-        aS.volume = 1 - Random.Range(0, 0.7f);
-        aS.PlayOneShot(currentFs);
-
+       
+        if (collision.gameObject.CompareTag("Grass"))
+        {
+            audioClipIndex = Random.Range(0, footstepsGrass.Count);
+            currentFs = footstepsGrass[audioClipIndex];
+        }
+        else if (collision.gameObject.CompareTag("Sand"))
+        {
+            audioClipIndex = Random.Range(0, footstepsSand.Count);
+            currentFs = footstepsSand[audioClipIndex];
+        }
+        else if (collision.gameObject.CompareTag("Concrete"))
+        {
+            audioClipIndex = Random.Range(0, footstepsConcrete.Count);
+            currentFs = footstepsConcrete[audioClipIndex];
+        }
+        else if (collision.gameObject.CompareTag("Water"))
+        {
+            audioClipIndex = Random.Range(0, footstepsWater.Count);
+            currentFs = footstepsWater[audioClipIndex];
+        }
 
     }
+    void playFootstepsSound()
+    {
+        aS.clip = currentFs;
+        aS.pitch = 1 + Random.Range(-0.2f, 0.2f);
+        aS.volume = 1 - Random.Range(0, 0.7f);
+        aS.PlayOneShot(currentFs);
+    }
+
 }
