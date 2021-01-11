e47826f2ac891978500c6a9e26ed4b863e4b5a3d
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-11 16:34:33 -0500

    Added LimitMovement script

---
+++ MFGJ-2021-January/Assets/Scripts/LimitMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitMovement : MonoBehaviour
+{
+    public float xLimit;
+    public float yLimit;
+
+    private Transform thisTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thisTransform.position.x > xLimit)
+        {
+            thisTransform.position = new Vector3(xLimit, thisTransform.position.y, thisTransform.position.z);
+
+        }
+
+        if (thisTransform.position.x < -xLimit)
+        {
+            thisTransform.position = new Vector3(-xLimit, thisTransform.position.y, thisTransform.position.z);
+
+        }
+        if (thisTransform.position.y > yLimit)
+        {
+            thisTransform.position = new Vector3(thisTransform.position.x, yLimit, thisTransform.position.z);
+
+        }
+
+        if (thisTransform.position.y < -yLimit)
+        {
+            thisTransform.position = new Vector3(thisTransform.position.x, -yLimit, thisTransform.position.z);
+
+        }
+    }
+}
