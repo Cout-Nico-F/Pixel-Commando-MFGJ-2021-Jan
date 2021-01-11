c09a773be3ccdbdbdf569a566d2dbab843f3c698
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-11 15:55:15 -0500

    Camera System Prototype

---
+++ MFGJ-2021-January/Assets/Scripts/Player/LimitMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitMovement : MonoBehaviour
+{
+    public float xLimit;
+    public float yLimit;
+
+    private Transform thisTransform;
+
+    
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        LimitObjectMovement();
+    }
+
+    void LimitObjectMovement()
+    {
+        if (thisTransform.position.x > xLimit)
+        {
+            thisTransform.position = new Vector3(xLimit, thisTransform.position.y, thisTransform.position.z);
+
+        }
+
+        if (thisTransform.position.x < -xLimit)
+        {
+            thisTransform.position = new Vector3(-xLimit, thisTransform.position.y, thisTransform.position.z);
+
+        }
+        if (thisTransform.position.y > yLimit)
+        {
+            thisTransform.position = new Vector3(thisTransform.position.x, yLimit, thisTransform.position.z);
+
+        }
+
+        if (thisTransform.position.y < -yLimit)
+        {
+            thisTransform.position = new Vector3(thisTransform.position.x, -yLimit, thisTransform.position.z);
+
+        }
+    }
+}
