37228311961e02ea9bc7c0ad8204bdd2abf1c682
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-14 13:15:15 -0500

    AudioManager V1.0 (test)

---
+++ MFGJ-2021-January/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Music Tracks")]
+    public AudioClip startScreenMx;
+    public AudioClip lvl1Mx;
+    public AudioClip lvl2Mx;
+
+
+    [Header ("Weapon Sounds")]
+    public AudioClip mcBulletSound;
+    public AudioClip enemiesBulletSound;
+    public AudioClip pickUpSound;
+
+    [Header("Audiosources")]
+    public AudioSource musicAudiosource;
+    public AudioSource weaponsAs;
+
+       
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void PlayBulletSound()
+    {
+        
+        weaponsAs.clip = mcBulletSound;
+        weaponsAs.volume = 1;
+        weaponsAs.pitch = 1;
+        weaponsAs.Play();
+        Debug.Log("Phew");
+
+    }
+}
