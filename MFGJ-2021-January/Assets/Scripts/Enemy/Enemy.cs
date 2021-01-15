c69e2a4a9d4f67eb6c1f41298f856e211985d203
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-01-15 18:50:18 -0300

    Enemy can die. part1

---
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int healthPoits = 100;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            healthPoits -= collision.GetComponent<Bulleting>().damage;
+        }
+    }
+
+    private void Update()
+    {
+        if (healthPoits <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
