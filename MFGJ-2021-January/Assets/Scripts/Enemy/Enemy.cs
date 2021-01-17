b1957a8545918e64e7ad95de4042bcc279b04e72
colorplease <alistairvtran@gmail.com>
2021-01-17 09:13:26 -0800

    Chasing Player Implemented

--- MFGJ-2021-January/Assets/Scripts/Enemy/Enemy.cs
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Enemy.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject deathPrefab;
     Animation hitAnimation;
     public int healthPoints = 100;
+
+    public Transform target;
+
+    NavMeshAgent agent;
+
+     public VariableController ooga1;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        GameObject g = GameObject.FindGameObjectWithTag("VariableController");
+        ooga1 = g.GetComponent<VariableController>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bullet"))
@@ -27,6 +43,11 @@ public class Enemy : MonoBehaviour
         {
             Destroy(this.gameObject);
             Instantiate(deathPrefab, this.transform.position, this.transform.rotation);
+        }
+
+        if (ooga1.ooga == true)
+        {
+            agent.SetDestination(target.position);
         }
     }
 }
