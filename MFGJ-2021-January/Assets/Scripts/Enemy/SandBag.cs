5e3133d20d12861da7e9b1ff0085d30f1012992f
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-01-20 16:33:29 -0300

    Fixed sandBag collider. Now it detects every hit correctly

--- MFGJ-2021-January/Assets/Scripts/Enemy/SandBag.cs
+++ MFGJ-2021-January/Assets/Scripts/Enemy/SandBag.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class SandBag : MonoBehaviour
 {
-    public float blockProbability = 0.5f;
+    public float blockProbability;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Sandbag collision detected:");
+
         if (collision.CompareTag("Bullet"))
         {
             float result = Random.value;
+            Debug.Log("Sandbag block num:" + result);
             if (result < blockProbability)
             {
                 Destroy(collision.gameObject);
