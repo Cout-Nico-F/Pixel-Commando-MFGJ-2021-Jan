8ea82513478599fef1c84cb6e415eb5c6b191b0a
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-01-25 22:59:49 -0300

    Explosion!

---
+++ MFGJ-2021-January/Assets/Explode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject explosion;
+    private void OnDestroy()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
