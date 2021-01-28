2ef4b3bb7ad543087ff1903abcbbc97cd4575575
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-01-28 18:56:18 -0300

    First Volume Slider. (issue: Shoots and groans are not in master mixer! )

---
+++ MFGJ-2021-January/Assets/Scripts/Audio/SetVolume.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    private Slider masterSlider;
+
+    private void Awake()
+    {
+        masterSlider = this.gameObject.GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("Master_Volume_Value", 0.5f);
+    }
+    public void Master_Volume(float sliderValue)
+    {
+        mixer.SetFloat("Master_Volume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Master_Volume_Value", sliderValue);
+    }
+
+}
+
