d8382a6210c193898534794a895fbc58c8b64988
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-01-28 21:32:00 -0500

    Updated Rocket_Blue explossion behavior.

--- MFGJ-2021-January/Assets/Scripts/Player/Bulleting.cs
+++ MFGJ-2021-January/Assets/Scripts/Player/Bulleting.cs
@@ -9,11 +9,26 @@ public class Bulleting : MonoBehaviour
 
     void Start()
     {
+        
         Invoke("DestroyBullet", lifeTime);
     }
 
     private void DestroyBullet()
     {
         Destroy(this.gameObject);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (this.gameObject.name == "Rocket_Blue(Clone)")
+        {
+            if (collision.gameObject.CompareTag("InfantryEnemy") || collision.gameObject.CompareTag("MachinegunEnemy"))
+            {
+                Debug.Log("BOOOOOM");
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
