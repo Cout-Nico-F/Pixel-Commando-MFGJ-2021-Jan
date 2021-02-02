f4413d3f479305beee3b286273dca19d0169c3b6
abernathyz <51379237+abernathyz@users.noreply.github.com>
2021-02-01 21:32:19 -0500

    Added enemy enable collider
    Added collider to enable/disable enemy sprites/scripts

---
+++ MFGJ-2021-January/Assets/Scripts/Player/EnableCollider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCollider : MonoBehaviour
+{
+    private CircleCollider2D myCollider;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myCollider = GetComponent<CircleCollider2D>();
+
+        Collider2D[] hitColliders;
+        hitColliders = Physics2D.OverlapCircleAll(transform.position, myCollider.radius);
+
+        for (int i = hitColliders.Length - 1; i > -1; i--)
+        {
+            EnableObject(hitColliders[i], true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnableObject(other, true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        EnableObject(other, false);
+    }
+
+    private void EnableObject(Collider2D other, bool state)
+    {
+        if (other.CompareTag("InfantryEnemy") || other.CompareTag("MachinegunEnemy"))
+        {
+            other.GetComponentInChildren<MonoBehaviour>(true).enabled = state;
+            other.GetComponent<MonoBehaviour>().enabled = state;
+            other.GetComponent<SpriteRenderer>().enabled = state;
+        }
+
+    }
+
+}
