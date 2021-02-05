74350cb746739c466da71ea32f5bc6f9b6ed2bd4
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-02-05 17:07:56 -0300

    Map toggles pause now. Avoided error with pause menu. Cant pause and map at time

--- MFGJ-2021-January/Assets/Scripts/Managers/GameManager.cs
+++ MFGJ-2021-January/Assets/Scripts/Managers/GameManager.cs
@@ -126,7 +126,7 @@ public class GameManager : MonoBehaviour
     }
     private void TogglePause()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        if ( (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P) ) && !MapCanvas.activeSelf )
         {
             if (Time.timeScale == 1)
             {
@@ -206,10 +206,14 @@ public class GameManager : MonoBehaviour
 
     private void ToggleMap()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M) && !PauseCanvas.activeSelf)
         {
             MapCanvas.SetActive(!MapCanvas.activeSelf);
-
+            if (Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+            }
+            else Time.timeScale = 1;
         }
     }
 }
