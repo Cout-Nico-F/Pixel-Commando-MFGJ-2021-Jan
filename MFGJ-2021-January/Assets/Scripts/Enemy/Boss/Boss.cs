79519238c66d4a587ef4690e7028f0b19dfd6128
IamGaelTP <IamGaelTP@users.noreply.github.com>
2021-02-06 16:51:23 -0600

    Boss Class
    -Boss Movement and Attacks.
    -Take Player Damage
    -Waiting for integrate animations
    
    ***ts a layour for soon bosses -> reusable***

---
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Boss/Boss.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField]
+    private bool isFlipped = false;
+
+    [Header("Variables")]
+    public int healthPoints = 200;
+    public int attackOneDamage = 0;
+    public int attackTwoDamage = 0;
+    //Add more variables as u need
+
+    //Look at player -> Flip Boss
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if(transform.position.x < player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x > player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+
+    //Take Damage
+    public void TakeDamage(int damage)
+    {
+        //Take Player Damage
+        healthPoints -= damage;
+
+        if(healthPoints <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    #region States
+    //CREATE FUCNTIONS OF BOSS ATTACKING
+    public void FirstAttack()
+    {
+        //First 'gun' -> first attack
+    }
+
+    public void SecondAttack()
+    {
+        //Second 'gun' -> second attack
+    }
+
+    //CREATE BOSS DEATH FUNCTION -> DESTROY
+    public void Death()
+    {
+        this.gameObject.GetComponent<Animator>().SetTrigger("Destroy");
+        Destroy(this.gameObject);
+    }
+    #endregion
+
+}
