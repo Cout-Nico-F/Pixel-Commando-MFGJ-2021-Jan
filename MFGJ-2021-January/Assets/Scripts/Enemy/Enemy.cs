8fe19482e9ac2023672ea8b274d5e5f71e6ba2cc
IamGaelTP <IamGaelTP@users.noreply.github.com>
2021-02-07 22:24:49 -0600

    Revert "Boss Shooting"
    This reverts commit 9d585cad5fd271180f3277e74b93909dc58dce98.

--- MFGJ-2021-January/Assets/Scripts/Enemy/Enemy.cs
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Enemy.cs
@@ -122,7 +122,7 @@ public class Enemy : MonoBehaviour
             }
         }
     }
-    public void TryShoot()
+    private void TryShoot()
     {
         bool playerInRange = false;
         if (player != null)
