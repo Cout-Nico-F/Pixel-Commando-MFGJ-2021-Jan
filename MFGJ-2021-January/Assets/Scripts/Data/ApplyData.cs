bd87d29dca8cf0f00e3a7e3f0aadb6c95af97347
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-02-12 05:07:12 -0300

    Patch to avoid erorrs on console

--- MFGJ-2021-January/Assets/Scripts/Data/ApplyData.cs
+++ MFGJ-2021-January/Assets/Scripts/Data/ApplyData.cs
@@ -17,7 +17,7 @@ public class ApplyData : MonoBehaviour
 
     public void Update()
     {
-        if(repeat == 0)
+        if(repeat == 0 && menuManager !=null)//To avoid editor errors in the console
         {
             //Create or Load Files
             if (menuManager.isNewGame) CreateFile();
