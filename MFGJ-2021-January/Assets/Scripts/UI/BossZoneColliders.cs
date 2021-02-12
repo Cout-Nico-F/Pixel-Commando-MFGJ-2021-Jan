f88b5dcd5b6145afb4414d1547c95dabb1e4b20d
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-02-12 20:03:34 -0300

    Working in progress ( boss zone collider)

---
+++ MFGJ-2021-January/Assets/Scripts/UI/BossZoneColliders.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZoneColliders : MonoBehaviour
+{
+    bool canPass = false;
+
+    // this.GetComponent<EdgeCollider2D>().isTrigger = true;
+
+    public void toggleColor()
+    {
+        if (canPass)
+        {
+            canPass = false;
+        }
+       else canPass = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (canPass && collision.CompareTag("Player"))
+        {
+            toggleColor();
+        }
+    }
+}
