1e402dfd6e67d2d6ab94d50179c6102d4f1d8b13
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-02-15 15:29:56 -0300

    Boss Spawner remake

---
+++ MFGJ-2021-January/Assets/BossSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    public GameObject boss;
+
+    private void Awake()
+    {
+        boss.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            boss.SetActive(true);
+        }
+    }
+}
