41cade0e0e7b91abc41edc574e1e9084932e718e
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-03-23 22:29:25 -0300

    Difficulty setter taking shape already

--- MFGJ-2021-January/Assets/DifficultySetter.cs
+++ MFGJ-2021-January/Assets/DifficultySetter.cs
@@ -6,7 +6,7 @@ public class DifficultySetter : MonoBehaviour
 {
     private GameManager gm;
     private PlayerController player;
-
+    private Enemy[] enemiesArray;
     private void Awake()
     {
         gm = FindObjectOfType<GameManager>();
@@ -14,18 +14,37 @@ public class DifficultySetter : MonoBehaviour
     }
     void Start()
     {
-        Enemy[] enemies = FindObjectsOfType<Enemy>();
-        Debug.LogError("ENEMY AMMOUNT: " + enemies.Length);
+        enemiesArray = FindObjectsOfType<Enemy>();
+        Debug.LogError("ENEMY AMMOUNT: " + enemiesArray.Length);
         switch (gm.Difficulty)
         {
             case 1:
-                player.GetComponent<PlayerController>().maxHealthPoints = 200;
-                player.GetComponent<PlayerController>().lives = 5;
-                foreach (Enemy enemy in enemies)
-                {
-                    enemy.healthPoints = 1;
-                }
+                TweakPlayer(health: 200, lives: 5);    
+                TweakEnemies(health_multiplicator: 0.65);
+                break;
+            case 2:
+                break;
+            case 3:
+                TweakPlayer(health: 80, lives: 2);
+                TweakEnemies(health_multiplicator: 1.65);
+                break;
+            default: 
                 break;
         }
     }
+
+    private void TweakEnemies(double health_multiplicator)
+    {
+        foreach (Enemy enemy in enemiesArray)
+        {
+            enemy.healthPoints = (int)(enemy.healthPoints * health_multiplicator);
+        }
+    }
+
+    private void TweakPlayer(int health, int lives)
+    {
+        player.GetComponent<PlayerController>().maxHealthPoints = health;
+        player.GetComponent<PlayerController>().lives = lives;
+    }
+
 }
