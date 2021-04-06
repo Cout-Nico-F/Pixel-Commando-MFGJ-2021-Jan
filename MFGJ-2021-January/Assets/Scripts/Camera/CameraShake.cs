508402e3d8c3ff9d210fb400471e507b62acc019
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-06 16:35:48 -0300

    CameraShake mockup

---
+++ MFGJ-2021-January/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    CameraFollow cameraFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollow = FindObjectOfType<CameraFollow>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Method()
+    {
+        var smooth = cameraFollow.GetSmoothPosition();
+    }
+}
