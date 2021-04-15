1982f8a3d74f951f113c3d06d7b0453a172164ac
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-15 17:38:46 -0300

    Creating 3type bomb system and IExplode interface

---
+++ MFGJ-2021-January/Assets/Scripts/Player/Explosives/Bomb.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour , IExplode //mono is needed here?
+{
+    public void Detonate()
+    {
+        //Bomb cant be detonated. Its timer based.
+    }
+
+    public void Plant()
+    {
+        //Instantiate a bomb prefab in front of the player
+        //bomb prefab will contain animation and sound
+        //bomb prefab has a timer, then explodes.
+    }
+}
---
+++ MFGJ-2021-January/Assets/Scripts/Player/Explosives/Explosives.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosives : MonoBehaviour
+{
+    private bool hasBombs = false;
+
+    private bool bombIsPlanted = false;
+
+    private IExplode explosive;
+
+    public bool HasBombs { get => hasBombs; set => hasBombs = value; }
+    public bool BombIsPlanted { get => bombIsPlanted; set => bombIsPlanted = value; }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (bombIsPlanted)
+            {
+                explosive.Detonate();
+                bombIsPlanted = false;
+            }
+            else
+            {
+                if (hasBombs) //hasbombs turn true when you pick up a bomb-Item
+                {
+                    explosive.Plant();
+                    bombIsPlanted = true;
+                    hasBombs = false;
+                }
+                else //has no ammo
+                {
+                    //Shake or Animate the Bomb UI square
+                }
+            }
+        }
+    }
+}
