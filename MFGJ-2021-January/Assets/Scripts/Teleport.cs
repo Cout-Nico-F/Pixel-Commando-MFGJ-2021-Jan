1595963cbffdc0d61cc2bc4680fd4a05021a9e8d
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-17 16:21:36 -0300

    Working on the House system (polished teleport and Bg)

--- MFGJ-2021-January/Assets/Scripts/Teleport.cs
+++ MFGJ-2021-January/Assets/Scripts/Teleport.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
@@ -8,22 +6,33 @@ public class Teleport : MonoBehaviour
     private Transform teleportPosition;
     [SerializeField]
     private bool isExit;
+    float normalSize;
+    float normalSpeed;
 
     [SerializeField] private float RightLimit;
     [SerializeField] private float LeftLimit;
     [SerializeField] private float UpLimit;
     [SerializeField] private float DownLimit;
 
+    private void Awake()
+    {
+        normalSize = Camera.main.orthographicSize;
+        normalSpeed = FindObjectOfType<CameraFollow>().SmoothSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             //turn camera follow Smooth effect off.
-            float speed = FindObjectOfType<CameraFollow>().SmoothSpeed;
             Camera.main.GetComponent<CameraFollow>().SmoothSpeed = 0;
+
             if (isExit)
             {
                 Camera.main.GetComponent<LimitMovement>().ResetConfiguration();
+                Camera.main.transform.position = teleportPosition.position;
+                //zoom out
+                Camera.main.orthographicSize = normalSize;
             }
             else
             {
@@ -31,12 +40,14 @@ public class Teleport : MonoBehaviour
                 Camera.main.GetComponent<LimitMovement>().xLimit_Left = LeftLimit;
                 Camera.main.GetComponent<LimitMovement>().yLimit_Up = UpLimit;
                 Camera.main.GetComponent<LimitMovement>().yLimit_Down = DownLimit;
+                //zoom in
+                Camera.main.orthographicSize *= 0.5f;
             }   
             //move player to desired position
             collision.transform.position = teleportPosition.position;
 
             //return camera follow to normal.
-            Camera.main.GetComponent<CameraFollow>().SmoothSpeed = speed;
+            Camera.main.GetComponent<CameraFollow>().SmoothSpeed = normalSpeed;
         }
     }
 }
