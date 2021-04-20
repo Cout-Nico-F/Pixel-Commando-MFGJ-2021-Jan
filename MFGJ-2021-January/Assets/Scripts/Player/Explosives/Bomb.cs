114f11c74eee01433314dd2eb042f0fcfba3605e
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-19 22:27:53 -0300

    bomb offset to prevent Z order problems

--- MFGJ-2021-January/Assets/Scripts/Player/Explosives/Bomb.cs
+++ MFGJ-2021-January/Assets/Scripts/Player/Explosives/Bomb.cs
@@ -18,9 +18,10 @@ public class Bomb : MonoBehaviour , IExplode
 
     public void Plant()
     {
+        Vector3 offset = new Vector3(0, -0.25f, 0);
         gunning = FindObjectOfType<Gunning>();//Refresh the gunning reference because it changes on gun swapping.
         //Instantiate a bomb prefab in front of the player
-        Instantiate(deployedBomb, gunning.transform.position, Quaternion.identity);
+        Instantiate(deployedBomb, gunning.transform.position + offset, Quaternion.identity);
         //bomb prefab will contain animation and sound
         //bomb prefab has a timer, then explodes.
         //bomb prefab sets bombisplanted to false before exploding
