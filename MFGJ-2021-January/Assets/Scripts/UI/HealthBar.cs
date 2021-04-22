be8008e5dd1f8620f52f40ca1b196fc79266da81
Javiotic <xlonadarx@gmail.com>
2021-04-22 18:36:42 +0200

    Health bar fix with offset
    Fixes the Player 1 prefab's HealthSlider. Also adds an offset (healthSliderCorrection) to make the health visually lower when the player is about to die.

--- MFGJ-2021-January/Assets/Scripts/UI/HealthBar.cs
+++ MFGJ-2021-January/Assets/Scripts/UI/HealthBar.cs
@@ -8,12 +8,15 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Color lowColor;
     public Color highColor;
-
+    //A health bar with 10 health looks very big, even though you are 1 shot
+    //from dying. This "slider correction" makes the health look lower so the 
+    //player knows they're about to die.
+    private int healthSliderCorrection = 5;
 
     public void SetHealth(float health, float maxHealth)
     {
         slider.gameObject.SetActive(health < maxHealth);
-        slider.value = health;
+        slider.value = health - healthSliderCorrection;
         slider.maxValue = maxHealth;
         slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(lowColor, highColor, slider.normalizedValue);
     }
