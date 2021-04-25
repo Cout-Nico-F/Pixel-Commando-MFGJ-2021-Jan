ec91d1819cd56faf3d1c91aaf9b06ff8ca0ccb50
Javiotic <xlonadarx@gmail.com>
2021-04-25 11:20:30 +0200

    Create vehicle trigger-spawner
    Created VehicleSpawner prefab. Vehicles can now sweep in when the player activates a trigger.
    
    Everything about the vehicle is now in */Vechicle/*

---
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Vehicle/VehicleSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Animator animVehicleSpawner;
+
+    private void Awake() {
+        animVehicleSpawner = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("player entered");
+            animVehicleSpawner.SetBool("anim_playerInRange", true);
+        }
+    }
+}
