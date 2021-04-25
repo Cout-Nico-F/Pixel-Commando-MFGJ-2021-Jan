1730358299fb14dbb6d3010d8f634855db55d0b0
Javiotic <xlonadarx@gmail.com>
2021-04-25 15:24:25 +0200

    Fixes infinite spawning of dead prefabs on VehicleSpawner

--- MFGJ-2021-January/Assets/Scripts/Enemy/Vehicle/VehicleSpawner.cs
+++ MFGJ-2021-January/Assets/Scripts/Enemy/Vehicle/VehicleSpawner.cs
@@ -7,15 +7,23 @@ public class VehicleSpawner : MonoBehaviour
     // Start is called before the first frame update
 
     Animator animVehicleSpawner;
+    Enemy enemy;
+    bool isVehicleDead;
+    
 
     private void Awake() {
         animVehicleSpawner = GetComponentInChildren<Animator>();
+        enemy = GetComponentInChildren<Enemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemy.IsDead)
+        {
+            Debug.Log("Child enemy is dead, disabling gameobject.");
+            animVehicleSpawner.SetBool("anim_isDead", true);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
