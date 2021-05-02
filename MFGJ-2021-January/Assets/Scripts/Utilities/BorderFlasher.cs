4b21c365148da5ca88d3f3535a88b632be001c28
Javiotic <xlonadarx@gmail.com>
2021-05-02 13:05:56 +0200

    Simplify Border Flasher code, and use animations instead

--- MFGJ-2021-January/Assets/Scripts/Utilities/BorderFlasher.cs
+++ MFGJ-2021-January/Assets/Scripts/Utilities/BorderFlasher.cs
@@ -6,31 +6,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BorderFlasher: MonoBehaviour
 {
-    Image img;
-    // Start is called before the first frame update
-    Color transparent = new Color (1f, 0f, 0f, 0f);
-    Color visible = new Color (1f, 1f, 1f, 0.4f);
-
-    float duration;
-
-    private float startTime;
-    private float endTime;
-    private float t;
-    Color flashColor;
     Image damage, heal;
-    Animator healAnimator;
+    Animator healAnimator, damageAnimator;
 
     private void Start() {
         damage = transform.Find("Damage").GetComponent<Image>();
+        damageAnimator = transform.Find("Damage").GetComponent<Animator>();
         heal = transform.Find("Heal").GetComponent<Image>();
         healAnimator = transform.Find("Heal").Find("HealAnimation").GetComponent<Animator>();
-        img = damage;
-    }
-    
-    // Update is called once per frame
-    void Update() {
-        var t = ((Time.time - startTime) / (endTime-startTime) );
-        img.color = Color.Lerp(visible, transparent, t);
     }
 
     public void FlashBorder(string playerEvent)
@@ -38,21 +21,14 @@ public class BorderFlasher: MonoBehaviour
         switch (playerEvent)
         {
             case "damage":
-                duration = 0.5f;
-                img = damage;
-                img.color = visible;
+                damageAnimator.Play("Base Layer.FlashBorder", -1, 0f);
                 break;
             case "heal":
-                duration = 1f;
-                img = heal;
-                img.color = visible;
+                //duration = 1f;
                 healAnimator.Play("Base Layer.UpHealth", -1, 0f);
                 break;
             default:
-                img.color = transparent;
                 break;
         }
-        startTime = Time.time;
-        endTime = startTime + duration;
     }
 }
