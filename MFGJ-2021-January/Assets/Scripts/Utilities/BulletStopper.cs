e8f9f33cc13f94c701fcd1612236d616b2353262
Javiotic <xlonadarx@gmail.com>
2021-05-05 22:30:08 +0200

    Add a public GameObject to BulletStopper that can instantiate a bullet hit effect.

--- MFGJ-2021-January/Assets/Scripts/Utilities/BulletStopper.cs
+++ MFGJ-2021-January/Assets/Scripts/Utilities/BulletStopper.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class BulletStopper : MonoBehaviour
 {
+
+    public GameObject bulletHitEffect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bullet") || collision.CompareTag("Damage"))
         {
             //Debug.Log("Bullet stopped by " + this.gameObject.name);
+            if (bulletHitEffect){
+                Quaternion bulletRotation = Quaternion.Euler(0f, 0f, collision.transform.eulerAngles.z - 270);
+
+                GameObject _bulletHitEffect = GameObject.Instantiate(bulletHitEffect, collision.transform.position, bulletRotation) as GameObject;
+                Destroy(_bulletHitEffect, 0.5f);
+            }
             Destroy(collision.gameObject,0.05f);
         }
     }
