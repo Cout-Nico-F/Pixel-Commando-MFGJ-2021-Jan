5d6026afe942188b9ca6a73dafa4d2c8ea7fcfae
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-05-14 17:22:41 -0400

    Added Colliders in Houses.

--- MFGJ-2021-January/Assets/Scripts/Player/FeetCollisions.cs
+++ MFGJ-2021-January/Assets/Scripts/Player/FeetCollisions.cs
@@ -18,7 +18,7 @@ public class FeetCollisions : MonoBehaviour
         }
     }
 
-    /*private void OnTriggerStay2D(Collider2D collision)
+   /* private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "WoodInsideRoom")
         {
