d5af84c72a9d90fe2a52718aadcb8b756dc0f0f4
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-05-14 23:42:38 -0400

    Implementet ReverbZone inside quarters

---
+++ MFGJ-2021-January/Assets/Scripts/Audio/ReverbZoneOn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ReverbZoneOn : MonoBehaviour
+{
+    [SerializeField] AudioReverbZone m_ReverbZone;
+    [SerializeField] ReverbStatus m_ReverbStatus = new ReverbStatus();
+        
+
+    private void Start()
+    {
+        if(m_ReverbZone == null)
+        {
+            m_ReverbZone = FindObjectOfType<AudioReverbZone>();
+        }
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(m_ReverbStatus == ReverbStatus.Activate && !m_ReverbZone.isActiveAndEnabled)
+            {
+                m_ReverbZone.gameObject.SetActive(true);
+
+            }
+            else if (m_ReverbStatus == ReverbStatus.Deactivate && m_ReverbZone.isActiveAndEnabled)
+            {
+                m_ReverbZone.gameObject.SetActive(false);
+            }
+            
+        }   
+    }
+    
+}
+public enum ReverbStatus
+{
+    Activate,
+    Deactivate
+}
+
