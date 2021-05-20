0a4b670337d8fd8c67c393e1de91467aec6180a7
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-05-20 17:48:25 -0300

    Zoom System for rooms part1

---
+++ MFGJ-2021-January/Assets/Zoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject exit;
+    [SerializeField]
+    private GameObject entrance;
+
+    private bool playerInside;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (playerInside) //esta saliendo
+            {
+                playerInside = false;
+                RestoreZoom();
+                entrance.SetActive(true);
+            }
+            else //esta entrando
+            {
+                playerInside = true;
+                ApplyZoom();
+                exit.SetActive(true);
+            }
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    private void ApplyZoom()
+    {
+        //TODO: Method
+    }
+    private void RestoreZoom()
+    {
+        //TODO: Method
+    }
+
+}
