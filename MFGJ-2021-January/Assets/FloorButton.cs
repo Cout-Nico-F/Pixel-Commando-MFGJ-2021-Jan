4304aeaa4129f9bcedf24f859cd6fe81f5b9064e
Javiotic <xlonadarx@gmail.com>
2021-05-22 16:46:19 +0200

    WIP - StorageRoom puzzle assets and logic

---
+++ MFGJ-2021-January/Assets/FloorButton.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButton : MonoBehaviour
+{
+    public GameObject openingDoors;
+    public GameObject counter1;
+    public GameObject counter2;
+    public GameObject counter3;
+    private OpeningDoors openingDoorsController;
+
+    private float collisionTime;
+    private int objectsInTrigger;
+
+    private int buttonHoldCounter = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        openingDoorsController = openingDoors.GetComponent<OpeningDoors>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonHoldCounter(collisionTime);
+        CountDown(collisionTime);
+        //Debug.Log(collisionTime);
+
+        DoorTimerController(buttonHoldCounter);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("DeployedBomb"))
+        {
+            objectsInTrigger += 1;
+            Debug.Log(collision.name + " entered");
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("DeployedBomb"))
+        {
+            collisionTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("DeployedBomb"))
+        {
+            Debug.Log(collision.name + " exited");
+            objectsInTrigger -= 1;
+        }
+        
+        collisionTime = buttonHoldCounter;
+    }
+
+    // I'm pretty sure there's an inifitely more efficient way to do this.
+    private void ButtonHoldCounter(float iCollisionTime)
+    {
+        if (iCollisionTime <= 0f)
+        {
+            buttonHoldCounter = 0;
+
+            counter1.transform.Find("CounterDisabled").gameObject.SetActive(true);
+            counter1.transform.Find("CounterEnabled").gameObject.SetActive(false);
+        }
+        if (iCollisionTime > 0f)
+        {
+            buttonHoldCounter = 1;
+            counter1.transform.Find("CounterDisabled").gameObject.SetActive(false);
+            counter1.transform.Find("CounterEnabled").gameObject.SetActive(true);
+
+            counter2.transform.Find("CounterDisabled").gameObject.SetActive(true);
+            counter2.transform.Find("CounterEnabled").gameObject.SetActive(false);
+        }
+        if (iCollisionTime > 1f)
+        {
+            buttonHoldCounter = 2;
+            counter2.transform.Find("CounterDisabled").gameObject.SetActive(false);
+            counter2.transform.Find("CounterEnabled").gameObject.SetActive(true);
+
+            counter3.transform.Find("CounterDisabled").gameObject.SetActive(true);
+            counter3.transform.Find("CounterEnabled").gameObject.SetActive(false);
+        }
+        if (iCollisionTime > 2f)
+        {
+            buttonHoldCounter = 3;
+
+            counter3.transform.Find("CounterDisabled").gameObject.SetActive(false);
+            counter3.transform.Find("CounterEnabled").gameObject.SetActive(true);
+        }
+    }
+
+    private void CountDown(float iCollisionTime)
+    {
+        if (iCollisionTime > 0 && objectsInTrigger == 0)
+        {
+            collisionTime -= Time.deltaTime;
+        }
+    }
+
+    private void DoorTimerController(int ibuttonHoldCounter)
+    {
+        if (buttonHoldCounter > 0)
+        {
+            openingDoorsController.OpenDoors("Puzzle_GreenDoor");
+        }
+        else
+        {
+            openingDoorsController.CloseDoors("Puzzle_GreenDoor");
+        }
+    }
+
+}
---
+++ MFGJ-2021-January/Assets/OpeningDoors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningDoors : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDoors(string doorColor)
+    {
+        var doors = GameObject.FindGameObjectsWithTag(doorColor);
+        for (var i=0; i<doors.Length; i++)
+        {
+            var doorTransform = doors[i].GetComponent<Transform>().localScale;
+            doors[i].GetComponent<Transform>().localScale = new Vector3(doorTransform.x, 0.1f, doorTransform.z);
+        }
+    }
+
+    public void CloseDoors(string doorColor)
+    {
+        var doors = GameObject.FindGameObjectsWithTag(doorColor);
+        for (var i = 0; i < doors.Length; i++)
+        {
+            var doorTransform = doors[i].GetComponent<Transform>().localScale;
+            doors[i].GetComponent<Transform>().localScale = new Vector3(doorTransform.x, 1f, doorTransform.z);
+        }
+    }
+
+
+}
